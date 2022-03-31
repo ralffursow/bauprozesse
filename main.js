01681a8da0e900f64afb6e190f6e34671c5b655d
Ralf <ralf.fursow@student.jade-hs.de>
2022-03-31 17:15:14 +0200

    logo im header eingefügt & dateien umbenannt/hinzugefügt

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+const alleProzesse = JSON.parse('alleProzesse.json');
+console.log(alleProzesse);
+  
+const alleBauprozesse = [];
+const Bauprozess = {};
+for (let i = 0; i < alleProzesse; i++) {
+    switch (i % ) {
+        case 0: 
+
+    }
+}
+*/
