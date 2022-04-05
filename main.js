eff229f33f6c3dad26af6d398fc4e31452b96fed
hobbiejadehs <82370827+hobbiejadehs@users.noreply.github.com>
2022-04-05 15:54:15 +0200

    beispiel, wie ein digramm automatisch aus den vorhandenen prozessen erstellt werden kann

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+//das html element, wo das diagramm angezeigt wird
+const divMermaid = document.getElementById('ebene2');
+
+//hier werden alle prozesse gespeichert
+let alleProzesse = [];
 //die prozesse aus der lokalen datei alleProzesse.json lesen
 const getProzesse = async () => {
   const res = await fetch('./alleProzesse.json');
@@ -5,20 +10,54 @@ const getProzesse = async () => {
   return prozesse;
 };
 
+//gibt einen prozess anhand der id zurück
+const getProzessById = (id) => {
+  return alleProzesse.find((prozess) => prozess.id === id);
+};
+
+//die dauer eines prozesses als zufallstahl zwischen 1 und 10 hinzufügen
+const addDauer = (prozess) => {
+  prozess.dauer = Math.floor(Math.random() * 10) + 1;
+};
+
+//einen prozess in dem mermaid syntax erstellen
+const createMermaidProzess = (prozess, startdatum) => {
+  //nur ein startdatum hinzufügen, wenn eines existiert
+  const startStr = startdatum ? `${startdatum},` : '';
+  //die dauer in tagen
+  const tageCount = `${prozess.dauer}d`;
+  //den syntayx für mermaid erstellen
+  const mermaidSyntax = `${prozess.name}: ${startStr} ${tageCount}\n`;
+  //das ganze zu dem html element hinzufügen
+  divMermaid.innerHTML += mermaidSyntax;
+};
+
 getProzesse().then((prozesse) => {
-  console.log(prozesse);
+  alleProzesse = prozesse;
+  //es gibt zwei sichten (auftraggeber und auftragnehmer), daher müssen diese zuerst gefiltert werden (unterschiedliche prozesse)
+  let auftraggeberProzesse = alleProzesse.filter(
+    (prozess) => prozess.perspektive === 'Auftraggeber'
+  );
+  //nur die prozesse der zweiten ebene filtern
+  auftraggeberProzesse = auftraggeberProzesse.filter(
+    (prozess) => prozess.ebene === 2
+  );
+
+  //den startprozess auslesen (prozess der als einziger keinen vorgänger hat)
+  let startProzess = auftraggeberProzesse.find((prozess) => prozess.pre === '');
+
+  //die dauer für den startprozess hinzufügen
+  addDauer(startProzess);
+  createMermaidProzess(startProzess, '2022-03-22');
+
+  //die nachfolger (id) des startprozesses auslesen
+  let nachfolger = startProzess?.next.split(',');
+  //schleife über die nachfolger
+  for (let i = 0; i < nachfolger.length; i++) {
+    //der nachfolger wird ausgelesen
+    let nachfolgerProzess = getProzessById(parseInt(nachfolger[i]));
+    //die dauer für jeden prozess hinzufügen
+    addDauer(nachfolgerProzess);
+    createMermaidProzess(nachfolgerProzess);
+  }
 });
-//console.log(prozesse);
-//die prozesse in die tabelle einfügen
-
-// const alleProzesse = JSON.parse('alleProzesse.json');
-// console.log(alleProzesse);
-
-// const alleBauprozesse = [];
-// const Bauprozess = {};
-// for (let i = 0; i < alleProzesse; i++) {
-//     switch (i % ) {
-//         case 0:
-
-//     }
-// }
