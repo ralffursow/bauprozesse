5dac43f602744bf357de4d805d5be6e12e561fbe
hobbiejadehs <82370827+hobbiejadehs@users.noreply.github.com>
2022-04-05 13:32:40 +0200

    einlesen der prozesse mit einer funktion

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-/*
-const alleProzesse = JSON.parse('alleProzesse.json');
-console.log(alleProzesse);
-  
-const alleBauprozesse = [];
-const Bauprozess = {};
-for (let i = 0; i < alleProzesse; i++) {
-    switch (i % ) {
-        case 0: 
+//die prozesse aus der lokalen datei alleProzesse.json lesen
+const getProzesse = async () => {
+  const res = await fetch('./alleProzesse.json');
+  const prozesse = await res.json();
+  return prozesse;
+};
 
-    }
-}
-*/
+getProzesse().then((prozesse) => {
+  console.log(prozesse);
+});
+//console.log(prozesse);
+//die prozesse in die tabelle einfügen
+
+// const alleProzesse = JSON.parse('alleProzesse.json');
+// console.log(alleProzesse);
+
+// const alleBauprozesse = [];
+// const Bauprozess = {};
+// for (let i = 0; i < alleProzesse; i++) {
+//     switch (i % ) {
+//         case 0:
+
+//     }
+// }
